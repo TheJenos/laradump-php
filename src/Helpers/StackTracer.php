bf8dd6a32834acc59e907b93b0cda889d8bbd79c
Thanura Nadun <nadunnew@gmail.com>
2024-04-03 16:33:09 +0530

    Fix major issue

--- src/Helpers/StackTracer.php
+++ src/Helpers/StackTracer.php
@@ -3,6 +3,7 @@
 namespace Thejenos\Laradump\Helpers;
 
 use Spatie\Backtrace\Backtrace;
+use Throwable;
 
 class StackTracer
 {
@@ -10,8 +11,24 @@ class StackTracer
     {
         $backTrace = Backtrace::create()->frames();
         $lastTrace = collect($backTrace)->filter(function ($trace) {
-            return ! str_contains($trace->file, 'laradump')
-                && ! str_contains($trace->file, "laravel/framework");
+            return !str_contains($trace->file, 'laradump')
+                && !str_contains($trace->file, "laravel/framework");
+        })->first();
+
+        return [
+            'class' => $lastTrace->class,
+            'file_name' => basename($lastTrace->file),
+            'file_path' => $lastTrace->file,
+            'line' => $lastTrace->lineNumber,
+        ];
+    }
+
+    public static function createTraceForThrowable(Throwable $tr)
+    {
+        $backTrace = Backtrace::createForThrowable($tr)->frames();
+        $lastTrace = collect($backTrace)->filter(function ($trace) {
+            return !str_contains($trace->file, 'laradump')
+                && !str_contains($trace->file, "laravel/framework");
         })->first();
 
         return [
--- src/Observers/ExceptionObserver.php
+++ src/Observers/ExceptionObserver.php
@@ -5,6 +5,7 @@ namespace Thejenos\Laradump\Observers;
 use Illuminate\Foundation\Http\Events\RequestHandled;
 use Illuminate\Support\Facades\Event;
 use Spatie\Backtrace\Backtrace;
+use Thejenos\Laradump\Helpers\StackTracer;
 use Thejenos\Laradump\Helpers\VarDump;
 
 class ExceptionObserver extends Observer
@@ -12,17 +13,11 @@ class ExceptionObserver extends Observer
     public function register(): void
     {
         Event::listen(RequestHandled::class, function (RequestHandled $data) {
-            if ($data->response->exception === null) {
+            if (!isset($data) || !isset($data->response) || !isset($data->response->exception)) {
                 return;
             }
 
             $exception = $data->response->exception;
-
-            $backTrace = Backtrace::createForThrowable($exception)->frames();
-            $lastTrace = collect($backTrace)->filter(function ($trace) {
-                return ! str_contains($trace->file, 'laradump')
-                    && ! str_contains($trace->file, "laravel/framework");
-            })->first();
 
             laradump()->sendRequest([
                 'view' => view('laradump::log', [
@@ -32,12 +27,7 @@ class ExceptionObserver extends Observer
                         'color' => 'error',
                     ],
                     'context' => VarDump::customDumper($exception),
-                    'call' => [
-                        'class' => $lastTrace->class,
-                        'file_name' => basename($lastTrace->file),
-                        'file_path' => $lastTrace->file,
-                        'line' => $lastTrace->lineNumber,
-                    ],
+                    'call' => StackTracer::createTraceForThrowable($exception),
                 ])->render(),
             ]);
         });
